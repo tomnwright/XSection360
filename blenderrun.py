7d076147a1ab9cb12ec17641802f930f2e7bca83
Tom Wright <tomnwright@outlook.com>
2020-08-15 21:32:27 +0100

    Test cleanup

--- blenderrun.py
+++
@@ -1,9 +0,0 @@
-import bpy
-import sys
-
-filepath = bpy.path.abspath("//")
-sys.path.append(filepath)
-
-from init import register
-
-register()
--- progresstest.py
+++
@@ -1,5 +0,0 @@
-from tqdm import tqdm
-from time import sleep
-
-for i in tqdm(range(300), desc="Rendering...",ncols=120):
-    sleep(0.02)
