77f80d58b0a84cd3bb67cf3b3063cf0f17764a7f
Tom Wright <tomnwright@outlook.com>
2020-08-16 12:50:03 +0100

    __init__ documentation

--- XSection360/__init__.py
+++ XSection360/__init__.py
@@ -1,3 +1,30 @@
+"""
+XSection360 is a Blender addon for creating calculating relative cross-sectional
+of a 3D object, from different angles. It uses equirectangular projection to
+calculate and render camera positions for each pixel in an output image profile.
+
+The addon was written to create drag profiles of plane objects, for flight simulation.
+
+To Use:
+
+1) Install addon
+2) In the XSection360 Setup panel, set the target collection:
+        a collection containing all the objects to be included
+3) Click the Setup button - the scene will automatically be setup for XS360 processing
+4) Use the camera debugger to make sure all objects are in frame from every angle.
+        (Configure object position and camera ortho size)
+        (The camera rotates about the World Origin)
+5) Set an output txt file; click Run
+        ("Run in New Console" is recommended)
+        Addon renders an image from each camera position,
+        calculates the total lightness of the image pixels,
+        and outputs it to the output txt file.
+6) Wait for XSection360 Render to complete
+7) Process output txt using processing.py (see documentation)
+        Processes the txt file to generate a BW image
+"""
+
+
 bl_info = {
     "name": "XSection360",
     "description": "Create cross-sectional area image profile for calculating object drag from various angles.",
